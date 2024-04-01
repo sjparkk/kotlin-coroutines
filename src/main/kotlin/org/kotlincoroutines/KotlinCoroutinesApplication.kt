62bbc3c8c556f0673ec882a42842d25af4b3ce73
sjparkk <sjparkk0820@naver.com>
2024-04-01 23:23:17 +0900

    git init

---
+++ src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package org.kotlincoroutines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutinesApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCoroutinesApplication>(*args)
+}
---
+++ src/test/kotlin/org/kotlincoroutines/KotlinCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kotlincoroutines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutinesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
