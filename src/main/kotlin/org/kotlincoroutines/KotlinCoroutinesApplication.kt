11c2ead5d9b9ccee8bae2755f48994dec0e31160
sjparkk <sjparkk0820@naver.com>
2024-04-04 23:10:14 +0900

    feat : 호출(실행) 스레드를 블록하지 않아 메인 함수 종료 케이스

--- src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
+++ src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
@@ -1,11 +1,23 @@
 package org.kotlincoroutines
 
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinCoroutinesApplication
 
+@OptIn(DelicateCoroutinesApi::class)
 fun main(args: Array<String>) {
-    runApplication<KotlinCoroutinesApplication>(*args)
+
+    //GlobalScope.launch = 코루틴을 생성하기 위한 코루틴 빌더
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+//    Thread.sleep(2000L)
 }
