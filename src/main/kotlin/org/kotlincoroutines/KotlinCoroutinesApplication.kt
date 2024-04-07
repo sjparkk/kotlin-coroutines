433c00b38fd65f0e3545c38b3cca47f5d268bfa6
sjparkk <sjparkk0820@naver.com>
2024-04-07 22:36:44 +0900

    feat : job 인스턴스를 통한 코루틴 종료 대기

--- src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
+++ src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
@@ -11,12 +11,13 @@ class KotlinCoroutinesApplication
 fun main(args: Array<String>) = runBlocking { //코루틴 안에서는 runBlocking{} 의 사용은 권장 되지 않으며, 일반 적인 함수 코드 블록에서 중단 함수를 호출할 수 있도록 하기 위해 사용
 
     //GlobalScope.launch = 코루틴을 생성하기 위한 코루틴 빌더
-    GlobalScope.launch {
+    val job = GlobalScope.launch {
         delay(1000L)
         println("World!")
     }
 
     println("Hello,")
-    //코루틴 안에서만 사용 가능
-    delay(2000L)
+
+    //Job 인스턴스를 통해 작업 완료 대기
+    job.join()
 }
