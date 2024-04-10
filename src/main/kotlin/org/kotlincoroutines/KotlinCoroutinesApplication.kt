3768a35f462b2284b87fd35294307d7c7bd5a84e
sjparkk <sjparkk0820@naver.com>
2024-04-10 23:02:56 +0900

    feat : runBlocking 과 coroutineScope 차이

--- src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
+++ src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
@@ -11,9 +11,26 @@ fun main(args: Array<String>) = runBlocking { //코루틴 안에서는 runBlocki
 
     //runBlocking{} 코루틴 빌더등을 이용해 생성 된 코루틴 블록 안에서 launch{} 코루틴 빌더를 이용하여 새로운 코루틴을 생성하면 현재 위치한 부모 코루틴에 join()을 명시적으로 호출할 필요 없ㅇ이 자식 코루틴들을 실행하고 종료될 때까지 대기 할 수 있음
     launch {
-        delay(1000L)
-        println("World!")
+        delay(200L)
+        println("Task from runBlocking")
     }
 
-    println("Hello,")
+    //runBlocking{}와 달리 coroutineScope{} 는 자식들의 종료를 기다리는 동안 현재 스레드를 블록하지 않음
+    coroutineScope {
+        launch {
+            delay(500L)
+            println("Task from nested launch")
+        }
+        delay(100L)
+        println("Task from coroutine scope")
+    }
+    println("Coroutine scope is over")
+
+    /**
+    결과
+    Task from coroutine scope
+    Task from runBlocking
+    Task from nested launch
+    Coroutine scope is over
+     */
 }
