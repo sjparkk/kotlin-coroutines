7ff0e99f82297c27985df82bf694de7cc4cdb829
sjparkk <sjparkk0820@naver.com>
2024-04-13 22:40:40 +0900

    feat : 함수 분리

--- src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
+++ src/main/kotlin/org/kotlincoroutines/KotlinCoroutinesApplication.kt
@@ -7,30 +7,17 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class KotlinCoroutinesApplication
 
-fun main(args: Array<String>) = runBlocking { //코루틴 안에서는 runBlocking{} 의 사용은 권장 되지 않으며, 일반 적인 함수 코드 블록에서 중단 함수를 호출할 수 있도록 하기 위해 사용
-
-    //runBlocking{} 코루틴 빌더등을 이용해 생성 된 코루틴 블록 안에서 launch{} 코루틴 빌더를 이용하여 새로운 코루틴을 생성하면 현재 위치한 부모 코루틴에 join()을 명시적으로 호출할 필요 없ㅇ이 자식 코루틴들을 실행하고 종료될 때까지 대기 할 수 있음
+fun main(args: Array<String>) = runBlocking {
     launch {
-        delay(200L)
-        println("Task from runBlocking")
+        doWorld()
     }
+    println("Hello,")
+}
 
-    //runBlocking{}와 달리 coroutineScope{} 는 자식들의 종료를 기다리는 동안 현재 스레드를 블록하지 않음
-    coroutineScope {
-        launch {
-            delay(500L)
-            println("Task from nested launch")
-        }
-        delay(100L)
-        println("Task from coroutine scope")
-    }
-    println("Coroutine scope is over")
+//코루틴 내부에서 실행되는 중단 함수는 suspend 키워드를 함수명 앞에 붙여 만들 수 있다.
+suspend fun doWorld() {
 
-    /**
-    결과
-    Task from coroutine scope
-    Task from runBlocking
-    Task from nested launch
-    Coroutine scope is over
-     */
+    //일반 함수와 달리 호출 가능
+    delay(1000L)
+    println("World!")
 }
